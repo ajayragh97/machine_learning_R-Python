09e24e06b637e209871f6f131f0d492455e967c4
Ajay Ragh <ajayragh345@gmail.com>
2019-10-08 22:12:01 +0530

    created data preprocessing template for further uses

---
+++ preprocessing template/data preprocessing_template.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 18:29:25 2019
+
+@author: Ajay Ragh
+"""
+#Data Preprocessing
+
+#Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+dataset=pd.read_csv('Data.csv')
+x=dataset.iloc[:, :-1].values
+y=dataset.iloc[:, 3].values
+
+#Splitting dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,random_state=0)
+
+#feature scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.fit_transform(x_test) """
