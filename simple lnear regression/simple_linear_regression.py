3fc4e8a54d5dd0aff9a66e69f839bb92d5da16ae
Ajay Ragh <ajayragh345@gmail.com>
2019-10-09 20:00:47 +0530

    simple linear regression model

---
+++ simple lnear regression/simple_linear_regression.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 19:21:58 2019
+
+@author: Ajay Ragh
+"""
+
+#Simple linear regression
+#Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+dataset=pd.read_csv('Salary_Data.csv')
+x=dataset.iloc[:, :-1].values
+y=dataset.iloc[:, 1].values
+
+#Splitting dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3,random_state=0)
+
+#feature scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.fit_transform(x_test) """
+
+#fitting simple linear regression to training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+#predicting test set results
+y_pred = regressor.predict(x_test)
