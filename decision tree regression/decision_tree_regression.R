3589353fb6dcdb8e99818e4605dd093c9257f23f
Ajay Ragh <ajayragh345@gmail.com>
2019-10-16 21:29:46 +0530

    created decision tree regression model in R and python

--- decision tree regression/decision_tree_regression.R
+++ decision tree regression/decision_tree_regression.R
@@ -1,7 +1,6 @@
-#regression template
-
+#decision tree rgeression
 #import dataset
-dataset = read.csv('data.csv')
+dataset = read.csv('Position_Salaries.csv')
 #dataset = dataset[2:3]
 
 #splitting dataset into training set and test set
@@ -15,30 +14,24 @@ dataset = read.csv('data.csv')
 
 #fitting  regression model to dataset
 #create regressor hhere
-
+#uncomment next install line if package is not preinstalled
+#install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = Salary ~ Level , data = dataset, control = rpart.control(minsplit = 1))
 
 #predicting a new result with the  regression model
-y_pred = predict(regressor , data.frame(Level = 6.5)))
+y_pred = predict(regressor , data.frame(Level = 6.5))
 
 
 #Visualising polynomial regression results
-ggplot()+
-  geom_point(aes(x = dataset$Level , y = dataset$Salary), colour = 'red')+ 
-  geom_line(aes(x= dataset$Level , y = predict(regressor, newdata = dataset)),colour = 'blue')+
-  ggtitle('regression model results')+
-  xlab('xlabel')+
-  ylab('ylabel')
-
-#Visualising polynomial regression results(high resolution curve)
 library(ggplot2)
 #adding new extra levels
-x_grid = seq(min(dataset$Level),max(dataset$Level),0.1)
+x_grid = seq(min(dataset$Level),max(dataset$Level),0.01)
 ggplot()+
   geom_point(aes(x = dataset$Level , y = dataset$Salary), colour = 'red')+ 
   geom_line(aes(x= x_grid , y = predict(regressor, newdata = data.frame(Level = x_grid))),colour = 'blue')+
-  ggtitle('regression model results')+
+  ggtitle('decision tree regression model results')+
   xlab('xlabel')+
   ylab('ylabel')
-
 
 
--- decision tree regression/decision_tree_regression.py
+++ decision tree regression/decision_tree_regression.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 15 19:49:35 2019
+Created on Wed Oct 16 20:43:24 2019
 
 @author: Ajay Ragh
 """
 
-#SVR
 #Importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,31 +14,31 @@ import pandas as pd
 
 dataset=pd.read_csv('Position_Salaries.csv')
 x=dataset.iloc[:, 1:2].values
-y=dataset.iloc[:, 2:3].values
+y=dataset.iloc[:, 2].values
 
 #Splitting dataset into training set and test set
 """from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3,random_state=0)"""
 
 #feature scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
 sc_y = StandardScaler()
-x = sc_x.fit_transform(x)
-y = sc_y.fit_transform(y)
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.fit_transform(x_test)
+y_train = sc_y.fit_transform(y_train)
+y_test = sc_y.fit_transform(y_test) """
 
-#fitting dataset to support vector regression model
-from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
-regressor.fit(x, y)
+#fitting dataset to  regression model
+
 
 #predicting results with polynomial regression model
-y_pred = sc_y.inverse_transform(regressor.predict(sc_x.transform(np.array([[6.5]]))))
+y_pred = regressor.predict(x)
 
 #visualising the regression model results
 plt.scatter(x, y,color = 'red')
 plt.plot(x,regressor.predict(x),color = 'blue')
-plt.title('SVR model plot')
+plt.title('regression model plot')
 plt.xlabel('position level')
 plt.ylabel('salary')
 plt.show()
